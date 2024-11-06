1b40c4c990a56d94256d3bb656c18c3aafa4f6b6
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-11-06 15:10:21 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51QIAVIRtcf6uOsyJiZsSAzPO9OMVeTANg7NOxI3AYwnUmimPTtVf0dGRks8YjWVkkMXaDu0D7IVee6uGB6SYq1wq00qhw9oA1e',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
